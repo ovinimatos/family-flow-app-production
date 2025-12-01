6d7aed7006d44f25a5faf878e77c285f88f67b7f
ovinimatos <viniciusmatosdealmeida@gmail.com>
2025-11-30 23:56:17 -0300

    Ajuste arquivo Nex.config.js para eliminar  404

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    // Garante que o build entenda a estrutura
+    distDir: '.next',
+  };
+  
+  module.exports = nextConfig;
